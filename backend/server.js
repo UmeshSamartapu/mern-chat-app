72585ed79e727a36e8e31db2bee6167bbc7285e5
UmeshSamartapu <umeshsamartapu@gmail.com>
2024-04-21 19:07:02 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";  //package-imports
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+
+import authRoutes from "./routes/auth.routes.js";   //file-imports
+import messageRoutes from "./routes/message.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import userRoutes from "./routes/user.routes.js";
+
+
+const app = express();   //variables
+const PORT = process.env.PORT||5000;
+
+
+dotenv.config();
+
+
+
+
+//to parse the in-coming request with JSON payloads{ from req .body}
+app.use(express.json());  //this middle layer is used to extract body parts of fullName...etc
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoutes);  //middle-layer
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+// app.get("/", (req, res) => {
+//     //root route http://localhost:5000/
+//     res.send("Hello World!!!");
+// });
+
+
+app.listen(PORT,() => {
+    connectToMongoDB();
+    console.log(`server Running on port ${PORT}`);
+});
