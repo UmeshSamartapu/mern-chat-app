29eba78d372ec1644f8f6760a5e06dc87e7cad1d
UmeshSamartapu <umeshsamartapu@gmail.com>
2024-05-17 17:44:44 +0530

    4th part completed socket

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,67 @@
-import express from "express";  //package-imports
+// import express from "express";  //package-imports
+// import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+
+
+// import authRoutes from "./routes/auth.routes.js";   //file-imports
+// import messageRoutes from "./routes/message.routes.js";
+// import connectToMongoDB from "./db/connectToMongoDB.js";
+// import userRoutes from "./routes/user.routes.js";
+
+
+// const app = express();   //variables
+// const PORT = process.env.PORT||5000;
+
+
+// dotenv.config();
+
+
+
+
+// //to parse the in-coming request with JSON payloads{ from req .body}
+// app.use(express.json());  //this middle layer is used to extract body parts of fullName...etc
+// app.use(cookieParser());
+
+
+// app.use("/api/auth", authRoutes);  //middle-layer
+// app.use("/api/messages", messageRoutes);
+// app.use("/api/users", userRoutes);
+
+
+// // app.get("/", (req, res) => {
+// //     //root route http://localhost:5000/
+// //     res.send("Hello World!!!");
+// // });
+
+
+// app.listen(PORT,() => {
+//     connectToMongoDB();
+//     console.log(`server Running on port ${PORT}`);
+// });
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-
-import authRoutes from "./routes/auth.routes.js";   //file-imports
+import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.routes.js";
 
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import {app,server} from './socket/socket.js';
 
-const app = express();   //variables
-const PORT = process.env.PORT||5000;
-
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-
-
-
-//to parse the in-coming request with JSON payloads{ from req .body}
-app.use(express.json());  //this middle layer is used to extract body parts of fullName...etc
+app.use(express.json());
 app.use(cookieParser());
 
-
-app.use("/api/auth", authRoutes);  //middle-layer
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Hello World!!!");
-// });
-
-
-app.listen(PORT,() => {
+server.listen(PORT, () => {
     connectToMongoDB();
-    console.log(`server Running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,12 +4,12 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
     return (
-        <div className="border-r border-slate-500 p-4 flex-col">
-          <SearchInput />
-          <div className="divider px-3"></div>
-          <Conversations />
-          <LogoutButton />   
-        </div>
+      <div className='border-r border-slate-500 p-4 flex flex-col'>
+			<SearchInput />
+			<div className='divider px-3'></div>
+			<Conversations />
+			<LogoutButton />
+		</div>
     );
 };
 
